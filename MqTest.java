0fbd310b8e895deb82f4c0479a9d2ab16686cded
LoginingName <1398752165@qq.com>
2016-09-13 11:33:15 +0800

    Update MqTest.java

--- MqTest.java
+++ MqTest.java
@@ -35,65 +35,7 @@ public class MqTest {
 
     @Test
     public void test(){
-        //mqService.sendTextMsg("hello world!");
-
-        /*EmailMsg emailMsg = new EmailMsg();
-        emailMsg.setSubject("豆包网注册");
-        String[] to = new String[]{"ruimin.zhang@baodanyun-inc.com"};
-        String[] cc = new String[]{"puguang.yang@baodanyun-inc.com"};
-        String[] bcc = new String[]{"18310116761@qq.com"};
-
-        emailMsg.setTo(to);
-        emailMsg.setCc(cc);
-        emailMsg.setBcc(bcc);
-
-        emailMsg.setContent("MQ测试邮件");
-
-
-        mqService.sendObjMsg(emailMsg);*/
-
-        /*EmailMsg emailMsg = new EmailMsg();
-        emailMsg.setId(367);
-        mqService.sendObjMsg(emailMsg);*/
-
-
-        /*SmsMsg smsMsg = new SmsMsg();
-        smsMsg.setMobile("18310116761");
-        smsMsg.setContent("MQ测试短信");
-        mqService.sendObjMsg(smsMsg);*/
-
-        /*SmsMsg smsMsg = new SmsMsg();
-        smsMsg.setId(365);
-        mqService.sendObjMsg(smsMsg);*/
-
-        //oAH_qsqNIkpSSQ0m3mUJUQwt8rUU普光
-        //oAH_qshkKs9l1klRASDOU1CVnj3s张锐敏
-        //oAH_qslSfiAiaRXBAHzw3e64k93g吴峥桂
-        //oAH_qsm6yPTi8I3hv_03mq4lCMKk于涛
-
-       /* String toUser = "oAH_qshkKs9l1klRASDOU1CVnj3s";
-
-        WeChatMsg weChatMsg = new WeChatMsg();
-        weChatMsg.setToUser(toUser);
-
-        SysWarnTpl warnMsg = new SysWarnTpl();
-        warnMsg.setTouser(toUser);
-        //warnMsg.setUrl("http://weixin.qq.com/download");
-
-        warnMsg.setFirst("请注意豆包网系统报警内容");
-        warnMsg.setKeyword1("用户子系统");
-        warnMsg.setKeyword2(DateUtils.getNowTime());
-        warnMsg.setKeyword3("高");
-        warnMsg.setRemark("新用户创建失败");
-
-
-        weChatMsg.setContent(warnMsg.toJson());
-
-        mqService.sendObjMsg(weChatMsg);*/
-
-        /*WeChatMsg weChatMsg = new WeChatMsg();
-        weChatMsg.setId(371);
-        mqService.sendObjMsg(weChatMsg);*/
+        
 
     }
 
